465c6a42e106abe2d6fb5707482be31346d6c287
Hasibul Kabir <19726091+hkhasib@users.noreply.github.com>
2025-04-21 22:06:54 +0600

    published forms can be viewed publicly

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,14 @@ Route::get('/login', [AuthController::class, 'viewLogin'])->name('view.login');
 Route::post('/login', [AuthController::class, 'login'])->name('post.login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('/form/show/{id}', [FormController::class, 'show'])->name('form.show');
 
 Route::group(['middleware' => ['auth']], function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('form', FormController::class)->except('show');
     Route::get('/form/preview/{id}', [FormController::class, 'preview'])->name('form.preview');
-    Route::get('/form/show/{id}', [FormController::class, 'show'])->name('form.show');
+
     Route::get('/form/list', [FormController::class, 'fetchForms'])->name('form.list');
     Route::post('/form/draft', [FormController::class, 'draft'])->name('post.form.draft');
 //    Route::get('/form/{form}/edit', [FormController::class, 'edit'])->name('form.edit');
